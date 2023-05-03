2c213a79ab1e81d2dddfbefbe87ca63f8c712c8a
Rubie-Eurickah <eurickahmatata345@gmail.com>
2023-05-03 16:52:04 +0300

    Creating a react Sidebar navigation men. I am using React Router v6.4

---
+++ side-bar/src/components/SidebarData.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarDate = [
+    {
+        title: 'Home',
+        path: "/",
+        icons: <AiIcons.AiFillHome />,
+        cName: "nav-text",
+
+    },
+]
---
+++ side-bar/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Products from './routes/Products';
+import Home from './routes/Home';
+import Reports from './routes/Reports';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "products",
+    element: <Products />,
+  },
+  {
+    path: "reports",
+    element: <Reports />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
